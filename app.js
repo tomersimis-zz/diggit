3e26c424843a8b86a4ee750b8402ee9d4d7f6acf
Tomer Simis <tls@cin.ufpe.br>
2016-06-20 14:26:58 -0300

    Updated the process to build the graph

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var GitHubStrategy = require('passport-github').Strategy;
 var GitHubApi = require("github");
 var jsnx = require('jsnetworkx');
 var async = require('async');
+var redis = require("redis");
 
+// Express
 var app = express();
 
 // Constants
 const MAX_ROOT_DISTANCE = 3; // starts at 0
+const NODE_TTL = 120; // time to keep node cache (in minutes)
+const USERS_PER_REQUEST = 3;
+const STARRED_PER_REQUEST = 1000;
+const WATCHED_PER_REQUEST = 1000;
 
 // Views configuration
 app.set('view engine', 'html');
@@ -26,6 +32,13 @@ app.use(require('cookie-parser')());
 app.use(require('express-session')({ secret: 'a7ff21426831e065916e21', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Redis
+client = redis.createClient();
+
+client.on("error", function (err) {
+    console.log("[REDIS ERROR] " + err);
+});
 
 passport.use(new GitHubStrategy({
 		clientID: '6a8a7ff214248ff9d900',
@@ -54,15 +67,20 @@ app.get('/', function(req, res) {
 	
 	if(req.user){
 		
-		buildGraph(req.user.username,function(graph){
+		buildGraph(req.user.username, function(graph){
 			console.log("--------------------------")
 			console.log("Nodes " + graph.numberOfNodes());
+			console.log(graph.nodes());
+
+			res.render('index', {
+				user: req.user
+			});
+
 		});
+	}else{
+		res.render('index');
 	}
 
-	res.render('index', {
-	  user: req.user
-	});
 });
 
 app.get('/logout', function(req, res){
@@ -85,6 +103,7 @@ app.listen(3000, function () {
   console.log('Server listening on port 3000.');
 });
 
+
 function buildGraph(root, callback){
 
 	var graph = new jsnx.DiGraph();
@@ -103,41 +122,116 @@ function buildGraph(root, callback){
 			return nodes.length > 0;
 		},
 		function(next){
-			var node = nodes.shift();
-			console.log(node);
-
-			if(graph.get(node.label).length > 0) { 
-				next();
-				return;
-			}
-
-			github.users.getFollowingForUser({
-				user: node.label
-			}, function(err, res) {
-				if(err) console.log(err);
-
-				for(var i in res){
-					if(!res[i].login) continue;
-					graph.addNode(node.label);
-					graph.addEdge(node.label, res[i].login);
-
-					if(node.level + 1 < MAX_ROOT_DISTANCE){
-						nodes.push({
-							label: res[i].login,
-							level: node.level + 1
-						});
-					}
-
-				}
-
-				next();
-
-			});
-
+			fetchNode(nodes, graph, next)
 		},
 		function(err){
 			callback(graph);
 		}
 	);
 
+}
+
+
+function fetchNode(nodes, graph, next){
+	var node = nodes.shift();
+	
+	if(graph.get(node.label).size > 0) {
+		console.log("[SKIPPING] " + node.label);
+		return next();
+	}
+
+	console.log("[PROCESSING] " + node.label + " - " + node.level);
+
+	client.hgetall(node.label, function(err, reply){
+		if(reply){
+			console.log("Cache hit");
+			var following = reply.following.split(',');
+			for(var i in following){
+				graph.addNode(following[i]);
+				graph.addEdge(node.label, following[i]);
+
+				if(node.level + 1 < MAX_ROOT_DISTANCE){
+					nodes.push({
+						label: following[i],
+						level: node.level + 1
+					});
+				}
+			}
+			next();
+		}else{
+			console.log("Cache miss");
+			buildNode(node, nodes, graph, next);
+		}
+	});
+
+}
+
+function buildNode(node, nodes, graph, next){
+
+	async.parallel({
+		following: function(callback){
+			github.users.getFollowingForUser({
+				user: node.label,
+				per_page: USERS_PER_REQUEST
+			}, function(err, res) {
+				if(err) console.log(err);
+				callback(null, res)
+			});
+		},
+		starred: function(callback){
+			github.activity.getStarredReposForUser({
+				user: node.label,
+				per_page: STARRED_PER_REQUEST
+			}, function(err, res){
+				if(err) console.log(err);
+				callback(null, res);
+			});
+		},
+		watched: function(callback){
+			github.activity.getStarredReposForUser({
+				user: node.label,
+				per_page: WATCHED_PER_REQUEST
+			}, function(err, res){
+				if(err) console.log(err);
+				callback(null, res);
+			});
+		}
+	},
+	function(err, results) {
+		var following = [];
+		for(var i in results.following){
+			if(!results.following[i].login || results.following[i].login.length == 0) continue;
+
+			following.push(results.following[i].login);
+			graph.addNode(node.label);
+			graph.addEdge(node.label, results.following[i].login);
+
+			if(node.level + 1 < MAX_ROOT_DISTANCE){
+				nodes.push({
+					label: results.following[i].login,
+					level: node.level + 1
+				});
+			}
+		}
+
+		var starred = [];
+		for(var i in results.starred){
+			starred.push(results.starred[i].full_name);
+		}
+
+		var watched = [];
+		for(var i in results.watched){
+			watched.push(results.watched[i].full_name);
+		}
+
+		client.hmset(node.label, ["following", following.toString(), "starred", starred.toString(), "watched", watched.toString()], function (err, res) {
+			if(err) return console.log(err);
+			client.expire(node.label, NODE_TTL*60, function(err, res){
+				if(err) return console.log(err);
+				next();
+			});
+		});
+	});
+
+	
 }
